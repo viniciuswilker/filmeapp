162528d7f9b82834e3fb3ba59adec6e1fc521d5f
Vinícius Wilker <159863481+viniciuswilker@users.noreply.github.com>
2025-07-21 20:24:36 -0300

    inicial

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl font-bold text-primary">Bem vindo.</Text>
+    </View>
+  );
+}
+
+
