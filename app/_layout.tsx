549844ff6e64742f06b6ef85e82fa9705fecee76
Vinícius Wilker <159863481+viniciuswilker@users.noreply.github.com>
2025-07-21 21:01:57 -0300

    menunavbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,13 @@
 import { Stack } from "expo-router";
-import './globals.css'
+import './globals.css';
 
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack >
+
+<Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+
+<Stack.Screen name="movie/[id]" options={{headerShown: false}}/>
+
+  </Stack>;
 }
